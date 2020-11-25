1fc2050b05e1b1d7c0840f7bd8ad416f63f52cbb
Aljosa Novak <aljosan123@gmail.com>
2020-11-25 16:18:44 +0100

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <b-navbar type="dark" variant="info">
+      <b-navbar-brand>Calendar</b-navbar-brand>
+      <b-navbar-nav>
+        <b-nav-item to="/">Home</b-nav-item>
+        <b-nav-item to="/about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  <div>
+    <router-view ></router-view>
+  </div>
   </div>
 </template>
 
@@ -15,18 +20,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
